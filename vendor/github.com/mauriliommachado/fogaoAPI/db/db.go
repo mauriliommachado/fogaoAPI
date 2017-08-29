0d165c7ed42c1f680af8f2126dc58e7d82e610c0
maurilio.machado <mauriliommachado@gmail.com>
2017-08-29 13:07:45 -0300

    a

--- vendor/github.com/mauriliommachado/fogaoAPI/db/db.go
+++ vendor/github.com/mauriliommachado/fogaoAPI/db/db.go
@@ -8,7 +8,7 @@ import (
 
 var fSession mgo.Session
 
-var mongo_url = "mongodb://heroku_j98w5qn4:heroku_j98w5qn4@ds161913.mlab.com:61913/heroku_j98w5qn4"
+var mongo_url = "mongodb://fogaoAdmin:fogaoAdmin@ds161913.mlab.com:61913/heroku_j98w5qn4"
 
 func Start() {
 
