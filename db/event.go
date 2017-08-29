c43dafdb8ab8e2c6888795776756ad70161895e7
Maurílio <mauriliommachado@gmail.com>
2017-08-29 02:39:28 -0300

    Initial commit

---
+++ db/event.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+type Event struct {
+	Id    bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Title  string `json:"title"`
+	Text string `json:"text"`
+	CreatedIn   time.Time `json:"createdIn"`
+	CreatedBy bson.ObjectId `json:"createdBy"`
+	Date  time.Time `json:"date"`
+	Range int `json:range`
+	Recipes []bson.ObjectId `json:"recipes"`
+	User bson.ObjectId `json:"user"`
+}
