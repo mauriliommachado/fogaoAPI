015813f519d728c9f44a8370bd221b8fe641f4ef
maurilio.machado <mauriliommachado@gmail.com>
2017-08-29 15:36:48 -0300

    asd

--- vendor/github.com/mauriliommachado/fogaoAPI/db/db.go
+++ vendor/github.com/mauriliommachado/fogaoAPI/db/db.go
@@ -8,7 +8,6 @@ import (
 var fSession mgo.Session
 
 func Start() {
-	fmt.Println("entrei")
 	session, err := mgo.Dial("mongodb://heroku_s9jcnfls:ekhui87a29vj9fdr88i0kf5t3v@ds161443.mlab.com:61443/heroku_s9jcnfls")
 	if err != nil {
 		panic(err)
